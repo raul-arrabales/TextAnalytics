dc06ab251c8d2162ba4c0146278d0dea354d987f
Raul Arrabales <raul.arrabales@gmail.com>
2016-10-27 11:07:40 +0200

    Update Scraper.py

--- Scraping/Scraper.py
+++ Scraping/Scraper.py
@@ -47,4 +47,34 @@ def clientthread(conn):
             print( "Error: %s" % e )
     conn.close()
 
-    
+    HOST = ''  
+PORT = 9994 # Arbitrary non-privileged port
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+print 'Socket created'
+
+#Bind socket to local host and port
+try:
+    s.bind((HOST, PORT))
+except socket.error , msg:
+    print 'Bind failed. Error Code : ' + str(msg[0]) + ' Message ' + msg[1]
+    sys.exit()
+
+print 'Socket bind complete'
+
+#Start listening on socket
+s.listen(10)
+print 'Socket now listening'
+
+
+
+#now keep talking with the client
+while 1:
+    #wait to accept a connection - blocking call
+    conn, addr = s.accept()
+    print 'Connected with ' + addr[0] + ':' + str(addr[1])
+
+    #start new thread takes 1st argument as a function name to be run, second is the tuple of arguments to the function.
+    start_new_thread(clientthread ,(conn,))
+
+s.close()
