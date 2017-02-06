3bf56048df827a90b00c68a5268752a57f001fba
Raul Arrabales <raul.arrabales@gmail.com>
2017-02-06 21:05:49 +0100

    Update Elasticsearch_Cmd.sh

--- search/Elasticsearch_Cmd.sh
+++ search/Elasticsearch_Cmd.sh
@@ -1,8 +1,20 @@
 
-curl -XPUT localhost:9200/test -d'{
-  "settings": {
-    "number_of_shards": 1,
-    "number_of_replicas": 0 }
-   }'
+# Get indices
+curl -X GET localhost:9200
+curl -X GET localhost:9200/_cat/indices
+
+# New index myindex
+curl -XPUT localhost:9200/myindex
+
+# New index test with settings
+curl -XPUT localhost:9200/test -d 
+ '{
+   "settings": {
+     "number_of_shards": 1,
+     "number_of_replicas": 0 
+     }
+  }'
    
+   
+
    
