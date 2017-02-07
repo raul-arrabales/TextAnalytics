d01e23afde676a3a1f0d3a256a58ccd7196219af
Raul Arrabales <raul.arrabales@gmail.com>
2017-02-07 12:16:27 +0100

    Create Solr_cmd.sh

---
+++ search/Solr_cmd.sh
@@ -0,0 +1,15 @@
+# Install / Update Java8
+sudo apt-get update
+sudo apt-get install default-jre
+
+# Download Solr
+# http://apache.rediris.es/lucene/solr/6.4.1/
+
+# Install Solr
+sudo ./install_solr_service.sh ../../solr-6.4.1.zip 
+
+# Browser to Solr admin
+# http://localhost:8983/solr/#/ 
+
+
+
