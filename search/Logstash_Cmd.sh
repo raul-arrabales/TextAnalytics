250bafcee32181f62d397ae7ca10e65ceb455527
Raul Arrabales <raul.arrabales@gmail.com>
2017-03-10 17:28:45 +0100

    Update Logstash_Cmd.sh

--- search/Logstash_Cmd.sh
+++ search/Logstash_Cmd.sh
@@ -1,1 +1,13 @@
 bin/logstash -f logstash-simple.conf 
+
+# Sending events from stdin: 
+Hola from logstash stdin input!
+{
+    "@timestamp" => 2017-03-10T16:19:25.876Z,
+      "@version" => "1",
+          "host" => "array-VirtualBox",
+       "message" => "Hola from logstash stdin input!"
+}
+
+# curl -XGET 'localhost:9200/logs*/_search?q="hola"&pretty&pretty'
+
